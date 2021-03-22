2401e4b5d0d1a513e5690969f663f55ee3674f72
Katherine Walendzik <katherinewalendzik@gmail.com>
2021-03-22 01:03:39 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,8 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  fruits = ["apples", "oranges", "plums", "pears"]
-  array.push("pineapple")
+  array.new = array.push("pineapple")
 end
 
 def add_element_to_start_of_array(array, element)
